b309ac84e092dd3bb6667ba5382b1a1b58d8de3f
Hendry Badao <hendrybadao@gmail.com>
2012-01-08 09:46:14 +0300

    Bugfix: typo in __site_width__ ini setting, causing incorrect 100% width

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -8,6 +8,6 @@ $lang['discussionPage']   = 'Discussion page (leave empty to disable discussions
 $lang['userPage']         = 'User page (leave empty to disable user pages)';
 $lang['sidebarID']        = 'page name of page included in sidebar';
 $lang['hideTools']        = 'Hide tools when not logged in?';
-$lang['nsWidth']          = 'Define site and sidebar width according to namespace.  Syntax: "<namespace> <site-width> <sidebar-width>", you can add other namespaces also, using a semicolon to separate';
+$lang['nsWidth']          = 'Define site and sidebar width according to namespace.  Syntax: "namespace site-width sidebar-width", you can add other namespaces also, using a semicolon to separate';
 
 
