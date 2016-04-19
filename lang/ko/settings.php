21cacc3cc4a72c4f2cac5daf4de9d63f3a6e0c05
Myeongjin <aranet100@gmail.com>
2016-04-19 23:46:39 +0900

    translation update

---
+++ lang/ko/settings.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Korean language file for config
+ *
+ */
+
+$lang['discussionPage']   = '토론 문서 (토론을 비활성화하려면 비워 두세요)';
+$lang['userPage']         = '사용자 문서 (사용자 문서를 비활성화하려면 비워 두세요)';
+$lang['hideTools']        = '로그인하지 않을 때 도구를 숨길까요?';
+//Custom styles to allow different site/sidebar widths per namespace
+//Syntax: "namespace site-width sidebar-width;namespace-2 site-width-2 sidebar-width-2"
+//(semicolons between namespaces sections, spaces within a section
+$lang['nsWidth']          = '개별 이름공간에 대한 사이트/사이드바 너비를 설정합니다. 문법: "이름공간 사이트-너비 사이드바-너비;사이트-너비2 사이드바-너비2", 등...';
